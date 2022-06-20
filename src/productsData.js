508db8c3ee19a0dff04ca206b68b7be394e31564
quangduy02 <quangduy131351@gmail.com>
2022-06-20 22:21:29 +0700

    duy

--- src/productsData.js
+++ src/productsData.js
@@ -6,43 +6,43 @@ export default [
         "url" : "../assets/images/slider/home54.png"
     },
     {
-        "id" : 2,
+        "id" : "2",
         "name" : "Doraemon Chọn Lọc",
         "price" : 40 ,
         "url" : "../assets/images/slider/home55.png"
     },
     {
-        "id" : 3,
+        "id" : "3",
         "name" : "Doraemon Tranh Truyện Màu",
         "price" : 23 ,
         "url" : "../assets/images/slider/home56.png"
     },
     {
-        "id" : 4,
+        "id" : "4",
         "name" : "Doraemon Truyện Ngắn Tập 5",
         "price" : 18 ,
         "url" : "../assets/images/slider/home57.png"
     },        
     {
-        "id" : 5,
+        "id" : "5",
         "name" : "Doraemon Truyện Ngắn - Tập 6",
         "price" : 18 ,
         "url" : "../assets/images/slider/home58.png"
     },
     {
-        "id" : 6,
+        "id" : "6",
         "name" : "Nobita Và Hành Tinh Thú",
         "price" : 16 ,
         "url" : "../assets/images/slider/home59.png"
     },
     {
-        "id" : 7,
+        "id" : "7",
         "name" : "Doraemon - Kỉ Niệm Về Bà",
         "price" : 25 ,
         "url" : "../assets/images/slider/home60.png"
     },
     {
-        "id" : 8,
+        "id" : "8",
         "name" : "Doraemon Học Tập",
         "price" : 40 ,
         "url" : "../assets/images/slider/home61.png"
@@ -51,193 +51,193 @@ export default [
 
 
     {
-        "id" : 9,
+        "id" : "9",
         "name" : "10 Chuyên Đề Bồi Dưỡng",
         "price" : 27 ,
         "url" : "../assets/images/slider/home14.png"
     },
     {
-        "id" : 10,
+        "id" : "10",
         "name" : "Tiếng Anh 8 - Tập 1",
         "price" : 34 ,
         "url" : "../assets/images/slider/home15.png"
     },
     {
-        "id" : 11,
+        "id" : "11",
         "name" : "Tiếng Anh 9 - Tập 2",
         "price" : 107 ,
         "url" : "../assets/images/slider/home16.png"
     },
     {
-        "id" : 12,
+        "id" : "12",
         "name" : "Bộ SGK lớp 8",
         "price" : 103 ,
         "url" : "../assets/images/slider/home17.png"
     },        
     {
-        "id" : 13,
+        "id" : "13",
         "name" : "150 Bài Tập Tiếng Việt 4",
         "price" : 44 ,
         "url" : "../assets/images/slider/home18.png"
     },
     {
-        "id" : 14,
+        "id" : "14",
         "name" : "150 Bài Văn Hay Lớp 3",
         "price" : 39 ,
         "url" : "../assets/images/slider/home19.png"
     },
     {
-        "id" : 15,
+        "id" : "15",
         "name" : "155 Bài Làm Văn Tiếng Việt 4",
         "price" : 43 ,
         "url" : "../assets/images/slider/home20.png"
     },
     {
-        "id" : 16,
+        "id" : "16",
         "name" : "155 Bài Làm Văn Tiếng Việt 5",
         "price" : 40 ,
         "url" : "../assets/images/slider/home21.png"
     },
     {
-        "id" : 17,
+        "id" : "17",
         "name" : "Xe Đua Địa Hình Điều Khiển",
         "price" : 86 ,
         "url" : "../assets/images/slider/home22.png"
     },
     {
-        "id" : 18,
+        "id" : "18",
         "name" : "Ống Chì Guangbo (36 màu)",
         "price" : 22 ,
         "url" : "../assets/images/slider/home23.png"
     },
     {
-        "id" : 19,
+        "id" : "19",
         "name" : "Bút Sáp Màu (10 màu)",
         "price" : 10 ,
         "url" : "../assets/images/slider/home46.png"
     },
     {
-        "id" : 20,
+        "id" : "20",
         "name" : "Bút Sáp Màu G-star",
         "price" : 51 ,
         "url" : "../assets/images/slider/home47.png"
     },
     {
-        "id" : 21,
+        "id" : "21",
         "name" : "Bút Xóa Nước",
         "price" : 15 ,
         "url" : "../assets/images/slider/home48.png"
     },
     {
-        "id" : 22,
+        "id" : "22",
         "name" : "Sổ Lò Xo Cool B5 200 Trang",
         "price" : 35 ,
         "url" : "../assets/images/slider/home49.png"
     },
     {
-        "id" : 23,
+        "id" : "23",
         "name" : "Bút Chì Màu 18 Màu Có Tẩy",
         "price" : 33 ,
         "url" : "../assets/images/slider/home50.png" 
     },
     {
-        "id" : 24,
+        "id" : "24",
         "name" : "Chì Màu Swaans 12 Màu Dài",
         "price" : 51 ,
         "url" : "../assets/images/slider/home51.png" 
     },
     {
-        "id" : 25,
+        "id" : "25",
         "name" : "Xe Đua Địa Hình Điều Khiển",
         "price" : 204 ,
         "url" : "../assets/images/slider/home34.png"  
     },
     {
-        "id" : 26,
+        "id" : "26",
         "name" : "Đồ Chơi Xe Sắt Cứu Hỏa",
         "price" : 87 ,
         "url" : "../assets/images/slider/home35.png"  
     },
     {
-        "id" : 27,
+        "id" : "27",
         "name" : "Máy Bay Điều Khiển Từ Xa",
         "price" : 133 ,
         "url" : "../assets/images/slider/home36.png"  
     },
     {
-        "id" : 28,
+        "id" : "28",
         "name" : "Vali Bác Sĩ Mika Xanh",
         "price" : 164 ,
         "url" : "../assets/images/slider/home37.png"  
     },
     {
-        "id" : 29,
+        "id" : "29",
         "name" : "Mô Hình Lego Minecraft",
         "price" : 240 ,
         "url" : "../assets/images/slider/home38.png"  
     },
     {
-        "id" : 30,
+        "id" : "30",
         "name" : "Mô Hình Lego Friends",
         "price" : 173 ,
         "url" : "../assets/images/slider/home39.png"  
     },
     {
-        "id" : 31,
+        "id" : "31",
         "name" : "Con Quay Vô Cực",
         "price" : 93 ,
         "url" : "../assets/images/slider/home40.png"  
     },
     {
-        "id" : 32,
+        "id" : "32",
         "name" : "Đồ Chơi Con Quay",
         "price" : 93 ,
         "url" : "../assets/images/slider/home41.png"  
     },
     {
-        "id" : 33,
+        "id" : "33",
         "name" : "Bút Bi Hoa Hướng Dương",
         "price" : 27 ,
         "url" : "../assets/images/slider/home42.png"  
     },  
     {
-        "id" : 34,
+        "id" : "34",
         "name" : "Giá Đựng Bút Gỗ HX1039",
         "price" : 301 ,
         "url" : "../assets/images/slider/home25.png"  
     },  
     {
-        "id" : 35,
+        "id" : "35",
         "name" : "Khay Đựng Bút Sắt Comic",
         "price" : 34 ,
         "url" : "../assets/images/slider/home26.png" 
     },  
     {
-        "id" : 36,
+        "id" : "36",
         "name" : "Ống Cắm Bút Đôi",
         "price" : 27 ,
         "url" : "../assets/images/slider/home27.png" 
     },  
     {
-        "id" : 37,
+        "id" : "37",
         "name" : "Bút Kĩ Thuật sensor Xanh Nhạt",
         "price" : 42 ,
         "url" : "../assets/images/slider/home28.png"
     },  
     {
-        "id" : 38,
+        "id" : "38",
         "name" : "Bút Bi Exam Grade Xanh",
         "price" : 22 ,
         "url" : "../assets/images/slider/home29.png"
     },  
     {
-        "id" : 39,
+        "id" : "39",
         "name" : "Bút Mài Thầy Ánh SH0234",
         "price" : 42 ,
         "url" : "../assets/images/slider/home30.png"
     }, 
      {
-        "id" : 40,
+        "id" : "40",
         "name" : "Bút Mài Thầy Ánh SH022",
         "price" : 83 ,
         "url" : "../assets/images/slider/home31.png"
